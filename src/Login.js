44d6257fe72c5de6d0d89db07a53dde3738bc7e4
nguyendazero <nguyendazero@gmail.com>
2024-10-17 00:27:33 +0700

    fetch API Login from Spring Boot Application

---
+++ src/Login.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+
+
+const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+    const [jwt, setJwt] = useState("");
+    const [profile, setProfile] = useState(null);
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch("http://localhost:8080/signin",{
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({username, password}),
+            }
+        );   
+            if(response.ok){
+                const data = await response.json();
+                console.log(data);
+                setJwt(data.jwtToken);
+                setMessage("Login successful");
+                fetchUserProfile(data.jwtToken);
+            }else{
+                setMessage("Login Failed Please check your credentials");
+            }
+            
+        } catch (error) {
+            console.log("error: " + error); 
+            setMessage("An error occured. Please try again.");
+        }
+    };
+
+    const fetchUserProfile = async (token) => {
+        try {
+            const response = await fetch("http://localhost:8080/profile",{
+                method: "GET",
+                headers: {
+                    "Authorization": `Bearer ${token}`,
+                },
+            }
+        );   
+            if(response.ok){
+                const data = await response.json();
+                console.log(data);
+                setProfile(data);
+            }else{
+                setMessage("Fail to fetch the Profile");
+            }
+            
+        } catch (error) {
+            console.log("error: " + error); 
+            setMessage("An error occured. Please try again.");
+        }
+    };
+
+    return (
+        <div>
+            {!profile ? (
+            <form onSubmit={handleLogin}>
+                <div>
+                    <label>Username: </label>
+                    <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    />
+                </div>
+
+                <div>
+                    <label>Password: </label>
+                    <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+            ) : (
+                <div>
+                    <h3>User Profile</h3>
+                    <p>Username: {profile.username}</p>
+                    <p>Roles: {profile.roles.join(", ")}</p>
+                    <p>Message: {profile.message}</p>
+                </div>
+            )}
+            {message && <p>{message}</p>}
+            {jwt && <p>{jwt}</p>}
+        </div>
+    );
+}
+
+export default Login;
